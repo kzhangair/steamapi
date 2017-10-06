64ff7344ed90e9d40c3e50f28c583ae8513dac84
kzhangair <kzhangair@qq.com>
2017-10-06 12:07:22 +0800

    add bs4 to resolve html

--- steamdb.py
+++ steamdb.py
@@ -1,9 +1,13 @@
 import urllib2
 import urllib
-
+from bs4 import BeautifulSoup
+ 
 url = 'https://steamdb.info/apps/page1'
 req = urllib2.Request(url)
 req.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)')
 response = urllib2.urlopen(req)
-the_page = response.read()
-print the_page
+page = response.read()
+soup = BeautifulSoup(page, 'lxml')
+tbody = soup.find_all('tbody')[0]
+for child in tbody.children
+	print child.attrs['data-appid']
