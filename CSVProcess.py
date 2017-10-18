a0cf4f6af4817c3751b213693ce6e78465c681a2
kzhangair <kzhangair@qq.com>
2017-10-19 00:47:33 +0800

    'CSVProcess.py for washing dataSet to get washedDataSet.csv'

---
+++ CSVProcess.py
@@ -0,0 +1,15 @@
+import csv
+
+fo = open('DataSet.csv', 'r')
+fw = open('washedDataSet.csv', 'wb')
+
+csvReader = csv.reader(fo)
+csvWriter = csv.writer(fw,delimiter="\v",lineterminator="\a")
+count = 0                
+for row in csvReader:
+    count = count + 1
+    csvWriter.writerow(row)
+    fw.flush()
+print count
+fo.close()
+fw.close()
